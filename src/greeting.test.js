7fd36e909cf4e4026a5bdb508d9db188ec4614cd
Sheb <michael@sheb.rocks>
2022-12-09 10:36:32 +0100

    add more linting and formatting

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -1,37 +1,39 @@
 import { greeting } from "./greeting"; // colocation
 
-it("greeting returns greeting with name", () => {
-  //given -- vorbedingungen / was gebe ich rein
-  const name = "Felix";
-  const expected = "hello friend, Felix";
+describe("greeting", () => {
+  it("greeting returns greeting with name", () => {
+    //given -- vorbedingungen / was gebe ich rein
+    const name = "Felix";
+    const expected = "hello friend, Felix";
 
-  //when -- was ich testen will / welcher ablauf
-  const actual = greeting(name);
+    //when -- was ich testen will / welcher ablauf
+    const actual = greeting(name);
 
-  //then -- nachbedingung / was bekomme ich raus
-  expect(actual).toBe(expected);
-});
+    //then -- nachbedingung / was bekomme ich raus
+    expect(actual).toBe(expected);
+  });
 
-it("prints out hello friend if person is known", () => {
-  //given -- vorbedingungen / was gebe ich rein
-  const name = "Mareike";
-  const expected = "hello friend, Mareike";
+  it("prints out hello friend if person is known", () => {
+    //given -- vorbedingungen / was gebe ich rein
+    const name = "Mareike";
+    const expected = "hello friend, Mareike";
 
-  //when -- was ich testen will / welcher ablauf
-  const actual = greeting(name);
+    //when -- was ich testen will / welcher ablauf
+    const actual = greeting(name);
 
-  //then -- nachbedingung / was bekomme ich raus
-  expect(actual).toBe(expected);
-});
+    //then -- nachbedingung / was bekomme ich raus
+    expect(actual).toBe(expected);
+  });
 
-it("prints out hello stranger for unknown person", () => {
-  //given -- vorbedingungen / was gebe ich rein
-  const name = "Jessica";
-  const expected = "hello stranger, Jessica";
+  it("prints out hello stranger for unknown person", () => {
+    //given -- vorbedingungen / was gebe ich rein
+    const name = "Jessica";
+    const expected = "hello stranger, Jessica";
 
-  //when -- was ich testen will / welcher ablauf
-  const actual = greeting(name);
+    //when -- was ich testen will / welcher ablauf
+    const actual = greeting(name);
 
-  //then -- nachbedingung / was bekomme ich raus
-  expect(actual).toBe(expected);
+    //then -- nachbedingung / was bekomme ich raus
+    expect(actual).toBe(expected);
+  });
 });
